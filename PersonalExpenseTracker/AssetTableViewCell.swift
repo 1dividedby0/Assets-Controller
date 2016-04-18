934c3cdab02adec0a340dee8130b5d01e21bb2b0
Dhruv Mangtani <turbodhruv@gmail.com>
2016-04-18 10:08:35 -0700

    Minor project updates

---
+++ PersonalExpenseTracker/AssetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AssetTableViewCell.swift
+//  PersonalExpenseTracker
+//
+//  Created by Dhruv Mangtani on 4/18/16.
+//  Copyright © 2016 dhruv.mangtani. All rights reserved.
+//
+
+import UIKit
+
+class AssetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
